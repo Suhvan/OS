b5dc34df30d6c79d0a520b0e26f13a81d10ae4d0
Suhvan <suhvan4ik@ubuntu.ubuntu-domain>
2012-06-26 10:19:08 +0600

    4-1

---
+++ tasks vol 2/4/4.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/poll.h>
+
+#define TIMEOUT 1
+
+const char *printfErrorMessage = "some trouble with printf";
+int *endStream, *numbers, ret, count=0, countLive;
+char buf[128];
+struct pollfd *fds;
+
+void closeAndExit()
+{
+	free(endStream);
+	free(fds);
+	exit(EXIT_FAILURE);
+}
+	
+void tryRead()
+{
+	int i;
+	ret = poll(fds, count, TIMEOUT);
+	if(ret == -1)
+	{
+		if(fprintf(stderr, "%s\n","some trouble with poll")==-1)
+		{
+			perror(printfErrorMessage);
+		}
+		closeAndExit();
+	}
+	if(ret != 0)
+	{
+		for(i=0; i<count; i++)
+		{
+			if(!endStream[i])
+			{
+				if((fds[i].revents & POLLNVAL) || (fds[i].revents & POLLHUP) || (fds[i].revents & POLLERR))
+				{
+					endStream[i]=1;
+					countLive--;
+				}
+				else if(fds[i].revents & POLLIN)
+				{
+					int result = read(fds[i].fd, buf, 20);
+					if(result==-1)
+					{
+						if(fprintf(stdout, "%s %d\n","I can not read file", i)==-1)
+						{
+							perror(printfErrorMessage);
+							closeAndExit();
+						}
+						endStream[i]=1;
+						countLive--;
+					}
+					else if(result == 0)
+					{
+						endStream[i]=1;
+						countLive--;												
+					}
+					else
+					{
+						write(1, buf, result);
+					}	
+				}
+			}
+		}
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	int i;
+	int file;
+	endStream = (int*) malloc((argc-1)*sizeof(int));
+	fds = (struct pollfd *)malloc((argc-1)*sizeof(struct pollfd*));
+	if(endStream==NULL || fds==NULL)
+	{
+		puts("Error: not enough memory");
+		return 2;
+	}
+
+	for(i=0; i<argc; i++)
+	{
+		endStream[i]=0;
+	}
+	
+	for(i=1; i<argc; i++)
+	{
+		if(sscanf(argv[i], "%d", &file))
+		{
+			fds[count].fd = file;
+			fds[count++].events = POLLIN | POLLNVAL | POLLERR | POLLHUP;
+		}
+	}
+	countLive = count;
+	while(countLive)
+	{
+		tryRead();
+	}
+}
